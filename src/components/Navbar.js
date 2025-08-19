08437c56292a14ee5a72936a74ed3027484866ac
Yuvraj Gupta <yuvrajam12@gmail.com>
2025-08-19 11:06:13 +0530

    19 oct

---
+++ src/components/Navbar.js
@@ -0,0 +1,54 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Navbar(props) {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">
+          {props.title}
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/">
+                {props.aboutText}
+              </a>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn btn-primary" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
+Navbar.propTypes = { title: PropTypes.string, aboutText: PropTypes.string };
+// Navbar.defaultProps={title:"Yuvraj", aboutText: "About"}
